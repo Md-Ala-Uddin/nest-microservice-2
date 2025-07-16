2d1ce1b7f145522ad76977e263c3b6821c474130
MD. ALA UDDIN <alauddin.be@bulipetech.com>
2025-07-16 13:01:26 +0600

    update(users_app): update user service with mock data and update user_dto

--- apps/users/src/users/dto/user.dto.ts
+++ apps/users/src/users/dto/user.dto.ts
@@ -1,6 +1,12 @@
-export class User {
+export class UserDto {
   id: number;
   email: string;
   name: string;
   password: string;
+}
+
+export class PublicUserDto {
+  id: number;
+  email: string;
+  name: string;
 }
--- apps/users/src/users/users.service.ts
+++ apps/users/src/users/users.service.ts
@@ -1,26 +1,70 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private users: UserDto[] = [
+    {
+      id: 1,
+      email: 'user1@gmail.com',
+      password: 'user1pass',
+      name: 'user1',
+    },
+    {
+      id: 2,
+      email: 'user2@gmail.com',
+      password: 'user2pass',
+      name: 'user2',
+    },
+  ];
+
+  create(createUserDto: CreateUserDto): UserDto {
+    const newUser: UserDto = {
+      ...createUserDto,
+      id: this.users.length + 1,
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): UserDto[] {
+    return this.users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): UserDto | undefined {
+    return this.users.find((user) => user.id === id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, updateUserDto: UpdateUserDto): UserDto | undefined {
+    const oldUser = this.findOne(id);
+    if (!oldUser) {
+      return undefined;
+    }
+
+    const updatedUser: UserDto = {
+      ...oldUser,
+      ...updateUserDto,
+    };
+
+    this.users.splice(
+      this.users.findIndex((oldUser) => oldUser.id === id),
+      1,
+      updatedUser,
+    );
+
+    return updatedUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: number): UserDto | undefined {
+    const user = this.findOne(id);
+    if (!user) return undefined;
+    return this.users.splice(
+      this.users.findIndex((user) => user.id === id),
+      1,
+    )[0];
   }
 }
